912b614620e843953ad4b4ec370c6c70a7f9e213
RIM EL FILALI <rimo1902@gmail.com>
2022-07-09 13:54:42 -0700

    main.h file

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct ptypes - struct for choosing the right function depending
+ * on the format specifier passed to printf function
+ * @m: format specifier
+ * @f: pointer to function
+ */
+
+typedef struct ptypes
+{
+	char *m;
+	int (*f)(va_list a);
+
+} ptypes_t;
+
+
+int _printf(const char *format, ...);
+int print_c(va_list a);
+int print_s(va_list a);
+int print_percent(va_list a);
+int print_d(va_list a);
+int print_i(va_list a);
+int _putchar(char c);
+
+#endif /* MAIN.H */
