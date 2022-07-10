c696993d763c3960f2d7b89f63ae310ed0a2909c
RIM EL FILALI <rimo1902@gmail.com>
2022-07-10 02:47:58 -0700

    prchar.c file

---
+++ prchar.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_c - function that prints a character
+ * @a: the character
+ * Return: 1
+ */
+
+int print_c(va_list a)
+{
+	char m = (char)va_arg(a, int);
+	
+	_putchar(m);
+	return (1);
+
+}
+
+/**
+ * print_s - function that prints a string
+ * @a: the string
+ * Return: number of printed characters
+ */
+
+int print_s(va_list a)
+{
+	int n;
+	char *m = va_arg(a, char *);
+
+	if (m == NULL)
+	{
+		m = "(null)";
+	}
+	for (n = 0; m[n]; n++)
+	{
+		_putchar(m[n]);								}
+	return (n);
+}
